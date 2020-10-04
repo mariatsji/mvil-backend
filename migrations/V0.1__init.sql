afa3ef7fd4f2e815a5af925f80eb0ab8e8bdce72
Sjur Millidahl <sjur.millidahl@finn.no>
2020-10-04 09:16:30 +0200

    no equals in default sql value

--- migrations/V0.1__init.sql
+++ migrations/V0.1__init.sql
@@ -2,6 +2,6 @@ CREATE TABLE posts (
     post_id serial NOT NULL,
     "user" TEXT not null,
     post TEXT not null,
-    timestamp timestamptz NOT NULL default = now(),
+    timestamp timestamptz NOT NULL default now(),
     PRIMARY KEY (post_id)
 );
