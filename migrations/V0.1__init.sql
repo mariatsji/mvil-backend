a03fd41acbf6856878230d676d43c294b809fd12
Sjur Millidahl <sjur.millidahl@finn.no>
2020-10-04 09:11:22 +0200

    quote reserved postgres qord

--- migrations/V0.1__init.sql
+++ migrations/V0.1__init.sql
@@ -1,6 +1,6 @@
 CREATE TABLE posts (
     post_id serial NOT NULL,
-    user TEXT not null,
+    "user" TEXT not null,
     post TEXT not null,
     timestamp timestamptz NOT NULL default = now(),
     PRIMARY KEY (post_id)
